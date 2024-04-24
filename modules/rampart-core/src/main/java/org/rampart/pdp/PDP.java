a0852bd555583f1aad4f28ebb61ee28aee7280c2
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2024-04-24 08:06:44 +0530

    add pdp

---
+++ modules/rampart-core/src/main/java/org/rampart/pdp/PDP.java
@@ -0,0 +1,10 @@
+package org.rampart.pdp;
+
+/**
+ * The Policy Decision Point (PDP) interface.
+ * This is based on new AuthZEN group Authroization API model : https://github.com/openid/authzen/blob/main/api/authorization-api-1_0.md
+ */
+public interface PDP {
+
+
+}
